546f601b650903608a1d2d2b7dfbd11b0ef01c60
tom <tomvagish4@gmail.com>
2019-02-26 20:24:01 +0200

    socket.io work, chat work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './chat.service';
+import { parse } from 'url';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ChatService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private chat: ChatService) {}
   title = 'chatClient';
+
+  messegeContent: string;
+  allMessages: string[] = [];
+
+
+  ngOnInit() {
+
+this.chat.getMessages()
+  .subscribe((message) => {
+ this.allMessages.push(message);
+
+  });
+
+
+  }
+
+  sendMessage() {
+    this.chat.sendMessage(this.messegeContent);
+    this.messegeContent = null;
+  }
+
+
+
+
 }
